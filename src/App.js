b98cd61b464715778e2cfb3af0799277964d2269
Chrysilla Mayasari <chrysilla_mysr@hotmail.com>
2022-03-18 17:39:30 +0700

    add welcome message

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>Welcome to Gigih 2.0</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
